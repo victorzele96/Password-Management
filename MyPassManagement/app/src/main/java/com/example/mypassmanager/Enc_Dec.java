70343c1e0acc25f335b746efdb0587731e70bf54
victorzele96 <57448124+victorzele96@users.noreply.github.com>
2020-09-05 18:22:07 +0300

    create a class responsible for encrypting and decrypting data.

---
+++ MyPassManagement/app/src/main/java/com/example/mypassmanager/Enc_Dec.java
@@ -0,0 +1,22 @@
+package com.example.mypassmanager;
+
+public abstract class Enc_Dec {
+
+    /**
+     * Encrypting the data
+     * @param data
+     * @return Encrypted data
+     */
+    public String EncValue(String data){
+        return null;
+    }
+
+    /**
+     * Decrypting the data
+     * @param data
+     * @return Decrypted data
+     */
+    public String DecValue(String data){
+        return null;
+    }
+}
--- MyPassManagement/app/src/main/java/com/example/mypassmanager/Pass_table.java
+++ MyPassManagement/app/src/main/java/com/example/mypassmanager/Pass_table.java
@@ -3,12 +3,41 @@ package com.example.mypassmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Pass_table extends AppCompatActivity {
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pass_details);
+
+        this.listView = findViewById(R.id.dataView);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("victor");
+
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        this.listView.setAdapter(arrayAdapter);
+
+        this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Pass_table.this, "Clicked item " + position + "\nUser name: " + "\nPassword: ", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void factoryMethod(String method){
+
     }
 }
